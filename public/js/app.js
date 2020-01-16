351c12e9c9e873ecdba9c3be6d4e57a9bb5ede8c
SanduNicu <sandu.nicu94@gmail.com>
2020-01-16 12:38:25 +0200

    added frontend form and functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,33 @@
 console.log('Js loaded');
 
-const fetchData = () => {
-  fetch('http://localhost:3000/weather?address=bucharest')
+const fetchData = (location) => {
+  return fetch(`http://localhost:3000/weather?address=${location}`)
     .then((res) => res.json())
-    .then((aa) => console.log(aa))
 }
 
-fetchData();
+const form = document.querySelector('.weather-form');
+const search = document.querySelector('.weather-input')
+const locationParagraph = document.querySelector('h2.location')
+const resultsParagraph = document.querySelector('p.results')
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const location = search.value;
+  resultsParagraph.textContent = 'Loading...';
+  location.textContent = '';
+  fetchData(location)
+    .then((data) => {
+      if (data.error) {
+        resultsParagraph.textContent = data.error;
+        locationParagraph.textContent = '';
+      } else {
+        const { summary, temperature, precipProbability, location } = data;
+        const message = `${summary} Temperature: ${Math.round(temperature)}C. Precipitation: ${Math.round(precipProbability)}%`
+        locationParagraph.textContent = location;
+        resultsParagraph.textContent = message;
+
+      }
+
+    })
+  
+})
